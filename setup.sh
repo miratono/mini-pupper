ae9f6d81af7e4022bb98235b563c473ff34b247c
miratono <102727658+miratono@users.noreply.github.com>
2025-02-07 15:46:38 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,3 @@ set -e
 
 # Get current username
 CURRENT_USER=$(whoami)
-
-# 1. Update netplan configuration for wlan0
-sudo sed -i '/wlan0:/,/^[^ ]/ s/optional: true/optional: false/' /etc/netplan/50-cloud-init.yaml
